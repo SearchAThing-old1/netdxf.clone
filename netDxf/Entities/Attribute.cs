48c570120a76fa62a624ef4f7e1b9a5ab7c4ea7f
Lorenzo Delana <oss.devel@searchathing.com>
2016-06-26 23:08:09 +0200

    Revert "fixed missing IsVisible field copy for Attribute on Clone()"
    This reverts commit a0dc757e5d29391a7e6cdb41bfaaea5655309172.

--- netDxf/Entities/Attribute.cs
+++ netDxf/Entities/Attribute.cs
@@ -431,7 +431,6 @@ namespace netDxf.Entities
                 Position = this.position,
                 Flags = this.flags,
                 Rotation = this.rotation,
-                IsVisible = this.IsVisible,
                 Alignment = this.alignment
             };
 
